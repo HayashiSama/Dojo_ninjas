d693e98f49dd5e3880a04e30f3bd277c44c142c5
Peter Lin <ptrlin@ucdavis.edu>
2018-02-20 09:58:31 -0800

    init

---
+++ apps/dojo_ninjas/models.py
@@ -0,0 +1,16 @@
+  # Inside models.py
+from __future__ import unicode_literals
+from django.db import models
+
+
+class Dojo(models.Model):
+	name = models.CharField(max_length=255)
+	city = models.CharField(max_length=255)
+	state = models.CharField(max_length=255)
+	desc = models.TextField()
+
+class Ninja(models.Model):
+	dojo = models.ForeignKey(Dojo, related_name="ninjas")
+	first_name = models.CharField(max_length=255)
+	last_name = models.CharField(max_length=255)	
+
